87addaa4ecc2e6be3e09f85988f4b3594ed934c2
Vion-sis <79034712+Vion-sis@users.noreply.github.com>
2023-03-17 22:18:02 +0900

    updated restaurant location

--- src/SpecialMenu/SpecialMenu.js
+++ src/SpecialMenu/SpecialMenu.js
@@ -7,31 +7,31 @@ import menu from "../images/menu.png";
 
 function SpecialMenu(props) {
   const wineMapping = [
-    { name: "Draft Beer", price: "500¥" },
-    { name: "Tusker Beer", price: "750¥" },
-    { name: "Asahi Beer", price: "650¥" },
-    { name: "Carisberg", price: "600¥" },
-    { name: "Savanna Dry", price: "600¥" },
-    { name: "Japanese Sake", price: "900¥ " },
-    { name: "Glass Wine", price: "500¥" },
-    { name: "Highball", price: "450¥" },
+    { name: "Draft Beer", price: "500¥", desc: "AU | Bottle" },
+    { name: "Tusker Beer", price: "750¥", desc: "AU | Bottle" },
+    { name: "Asahi Beer", price: "650¥", desc: "FR | 750ml" },
+    { name: "Carisberg", price: "600¥", desc: "IE | 750ml" },
+    { name: "Savanna Dry", price: "600¥", desc: "FR | 750ml" },
+    { name: "Japanese Sake", price: "900¥ ", desc: "JP | 500ml" },
+    { name: "Glass Wine", price: "500¥", desc: "CA | 750ml" },
+    { name: "Highball", price: "450¥", desc: "JP | 750ml" },
   ];
 
   const softDrinkMappping = [
-    { name: "00long tea", price: "300¥" },
-    { name: "Grape juice", price: "300¥" },
-    { name: "coconut juice", price: "300¥" },
-    { name: "Guava Juice", price: "300¥" },
-    { name: "Mango juice", price: "300¥" },
-    { name: "coffee", price: "350¥" },
-    { name: "Tea", price: "350¥" },
-    { name: "Chai", price: "400¥" },
+    { name: "00long tea", price: "300¥", desc: "JP  |  1cup" },
+    { name: "Grape juice", price: "300¥", desc: "INDO  |  750ml" },
+    { name: "coconut juice", price: "300¥", desc: "KE  |  750ml" },
+    { name: "Guava Juice", price: "300¥", desc: "TZ  |  750ml" },
+    { name: "Mango juice", price: "300¥", desc: "CH  |  750ml" },
+    { name: "coffee", price: "350¥", desc: "IND  |  1cup" },
+    { name: "Tea", price: "350¥", desc: "BR  |  1cup" },
+    { name: "Chai", price: "400¥", desc: "LK  |  1cup" },
   ];
   return (
-    <div>
+    <div className="special-menu">
       <div className="heading ">
         <SubHeading title="Menu that fits your palatte" />
-        <h1>Drink's Special</h1>
+        <h1>Drinks Special</h1>
       </div>
       <div className="d-flex justify-content-evenly">
         <div className="menu-items">
@@ -44,13 +44,14 @@ function SpecialMenu(props) {
                     <span className="menu-wine-name"> {wine.name}</span>{" "}
                     <div className="menu-dash" /> {wine.price}
                   </p>
+                  <p className="menu-desc"> {wine.desc}</p>
                 </div>
               );
             })}
           </div>
         </div>
         <div className="munu-image">
-          <img src={menu} alt="menu picture" />
+          <img src={menu} alt="menu " />
         </div>
 
         <div className="menu-items">
@@ -63,6 +64,7 @@ function SpecialMenu(props) {
                     <span className="menu-wine-name"> {drinks.name}</span>{" "}
                     <div className="menu-dash" /> {drinks.price}
                   </p>
+                  <p className="menu-desc"> {drinks.desc}</p>
                 </div>
               );
             })}
